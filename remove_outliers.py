e3439b2e075e5cc579b59072ad492e0c86023e54
Anton Smetanin <aasmetanin@edu.hse.ru>
2021-11-04 21:48:41 +0300

    Update remove_outliers.py

--- remove_outliers.py
+++ remove_outliers.py
@@ -7,7 +7,6 @@
 def removing_outliers(column,frame, count = 0,total_outliers = 0, alpha = 1.5,max_iter = 5):
         """
         Метод удаления выбросов с помощью межквартильного размаха
-
         Параметры:
         - column: Имя столбца, где нужно найти выбросы
         - frame: Фрейм, где находится столбец для анализа
@@ -15,22 +14,21 @@ def removing_outliers(column,frame, count = 0,total_outliers = 0, alpha = 1.5,ma
         - total_outliers: Общая сумма выбросов, найденных за все циклы проверки
         - alpha: Коэфициент при вычислении интервала
         - max_iter: максимальное количество циклов функции
-
         Возвращет:
         - frame: Обработанный фрейм
         - leng: Количесвто выбросов на цикле
         - total_outliers: Общее количество выбросов
         """
         # Получим квантиль 25%
-        q25=np.array(frame[column].describe(percentiles=[.25,.75]).loc['25%'])
+        q25 = frame[column].quantile(0.25)
         # Получим квантиль 75%
-        q75=np.array(frame[column].describe(percentiles=[.25,.75]).loc['75%'])
+        q75 = frame[column].quantile(0.75)
         # Получим первую границу
-        first_part=q25-alpha*(q75-q25)
+        first_part = q25 - alpha * (q75 - q25)
         # Получим 2 границу
-        second_part=q75+alpha*(q75-q25)
+        second_part = q75 + alpha * (q75 - q25)
         # Инициализируем список для индексов, подготовленных к удалению
-        index_del= []
+        index_del = []
         # Получим все значения, удолитворяющих промежутку
         prep_frame = frame[(frame[column] > first_part) & (frame[column] < second_part)]
         # Подсчет количсва удаленных выбросов
@@ -43,8 +41,7 @@ def removing_outliers(column,frame, count = 0,total_outliers = 0, alpha = 1.5,ma
         del prep_frame
         # Проверим условия выхода
         if leng > 0 and count < max_iter:
-            frame,leng,total_outliers = removing_outliers(column,frame, count,total_outliers+leng)
+            frame,leng,total_outliers = removing_outliers(column,frame, count,total_outliers + leng)
         else:
             print('Количество строк, выбранных для удаления {}: {}. Количество итераций {}'.format(column,total_outliers,count),end = "\n\n")
         return frame,leng,total_outliers
-
